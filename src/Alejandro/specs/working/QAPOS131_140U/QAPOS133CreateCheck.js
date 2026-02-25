7655d0892e7b01183d9b2866fabe398126fb2911
dark850916 <46248527+AlejandroSoftwareDeveloper@users.noreply.github.com>
2026-02-25 06:41:41 -0500

    Adicion de elementos sin terminar 132 y 133

---
+++ src/Alejandro/specs/working/QAPOS131_140U/QAPOS133CreateCheck.js
@@ -0,0 +1,14 @@
+
+describe("Check if items change correctly status",()=>{
+
+    it("TC0001: Create check corretly",async()=>{
+
+    })
+
+    it("TC0002: Send item to kitchen",async()=>{
+    })
+
+    it("TC0002: Delete orders corretly",async()=>{
+    })
+
+})
